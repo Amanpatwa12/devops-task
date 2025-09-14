dff93d9e5f7d4e62e19c634eec737333e6478e43
Amanpatwa12 <102861752+Amanpatwa12@users.noreply.github.com>
2025-09-14 20:10:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static file
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'logoswayatt.png'));
 });
 
-// Listen on all network interfaces
+
 app.listen(3000, '0.0.0.0', () => {
   console.log('Server running on http://0.0.0.0:3000');
 });
